b4372e122f4faf4ca2ccd95467493d4364ca5aa1
WARLOCK77 <99019837+WARLOCK77@users.noreply.github.com>
2022-02-19 17:25:53 +0200

    Add files via upload

---
+++ Homework5.java
@@ -0,0 +1,41 @@
+package ua.levelup.homework.savchenkodmitriy;
+import java.util.Scanner;
+public class Homework5 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please choose from the following menu options:" +
+                "\n  1. Edit user data" +
+                "\n  2. Calculate matrices" +
+                "\n  3. Send email with results" +
+                "\ntype q for exit");
+        do {
+            String st = scan.nextLine();
+            try {
+                int input = Integer.parseInt(st);
+                if (input == 1 || input == 2 || input == 3) {
+                    System.out.println("Was chosen menu " + input);
+                    break;
+                } else if (st.length() > 1) {
+                    System.out.println("Please enter only one symbol!");
+                } else {
+                    System.out.println("Please enter the valid entry!");
+                }
+            } catch (NumberFormatException ex) {
+                if (st.equalsIgnoreCase("q")) {
+                    System.out.println("Exit");
+                    break;
+                } else {
+                    System.out.println("Please enter the valid entry!");
+                }
+            }
+        }
+        while (true);
+    }
+}
+
+
+
+
+
+
